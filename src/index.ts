c3c1fb750f86967d57b0d0c28ba2b57df40c11fd
Ed Nikolenko <4426698+ednikolenko@users.noreply.github.com>
2019-03-31 18:59:25 +0300

    feat: allowed mutations

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
-import { MutationPayload, Store } from 'vuex';
+import type { MutationPayload, Store } from 'vuex';
 
 export const DEFAULT_MUTATION_KEY = 'VSS_MUTATION';
 export const DEFAULT_SNAPSHOT_KEY = 'VSS_SNAPSHOT';
+export const UNIQUE_ID = `${Date.now()}-${Math.random()}`;
 
 export type CreatePluginParams = {
   mutationKey: string;
+  mutations: string[];
   snapshotKey: string;
+};
+
+export type Message = {
+  from: string;
+  mutation: MutationPayload;
 };
 
 export type State = Record<string, unknown>;
 
 export default ({
   mutationKey = DEFAULT_MUTATION_KEY,
+  mutations = [],
   snapshotKey = DEFAULT_SNAPSHOT_KEY
 }: CreatePluginParams) => (store: Store<State>) => {
   // Work only in browser
   if (typeof window === 'undefined') {
     return;
   }
+
+  let isLocked = false;
 
   // Replace state from last snapshot
   const snapshot: State | null = JSON.parse(window.localStorage.getItem(snapshotKey));
@@ -26,17 +36,23 @@ export default ({
   }
 
   store.subscribe((mutation: MutationPayload, state: State) => {
-    try {
-      window.localStorage.setItem(mutationKey, JSON.stringify(mutation));
-      window.localStorage.removeItem(mutationKey);
-
-      window.localStorage.setItem(snapshotKey, JSON.stringify(state));
-    } catch (error) {
-      throw new Error(error);
+    if (isLocked) {
+      return;
     }
+    if (!mutations.includes(mutation.type)) {
+      return;
+    }
+
+    window.localStorage.setItem(mutationKey, JSON.stringify(mutation));
+    window.localStorage.removeItem(mutationKey);
+
+    window.localStorage.setItem(snapshotKey, JSON.stringify(state));
   });
 
   window.addEventListener('storage', (event: StorageEvent) => {
+    if (isLocked) {
+      return;
+    }
     if (event.key !== mutationKey) {
       return;
     }
@@ -44,12 +60,17 @@ export default ({
       return;
     }
 
-    try {
-      const mutation: MutationPayload = JSON.parse(event.newValue);
+    const message: Message = JSON.parse(event.newValue);
+    if (message.from === UNIQUE_ID) {
+      return;
+    }
 
-      store.commit(mutation.type, mutation.payload);
+    try {
+      store.commit(message.mutation.type, message.mutation.payload);
     } catch (error) {
       throw new Error(error);
+    } finally {
+      isLocked = false;
     }
   });
 };
