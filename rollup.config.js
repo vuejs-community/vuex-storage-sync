598b278e937a018cdd9b54882054fed79387e408
Ed Nikolenko <4426698+ednikolenko@users.noreply.github.com>
2020-02-23 09:27:22 +0200

    feat: export typescript declarations

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,23 @@
-import rollupPluginJson from 'rollup-plugin-json';
-import rollupPluginTypeScript from 'rollup-plugin-typescript';
+import rollupPluginJson from '@rollup/plugin-json';
+import rollupPluginTypeScript from '@wessberg/rollup-plugin-ts';
 
 export default {
   experimentalCodeSplitting: true,
   input: './src/index.ts',
-  output: {
-    exports: 'named'
-  },
+  output: [
+    {
+      exports: 'named',
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      exports: 'named',
+      file: 'dist/index.cjs.js',
+      format: 'system',
+      sourcemap: true
+    }
+  ],
   plugins: [
     rollupPluginJson(),
     rollupPluginTypeScript()
